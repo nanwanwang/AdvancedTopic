f7cb4526c647de316e586d2beb9b0a70fbb51452
demonwang <nanwanwang@hotmail.com>
2021-12-06 17:41:00 +0800

    add mutex

---
+++ MutexSample/MutexExample.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutexSample
+{
+    internal class MutexExample
+    {
+        static Mutex _m;
+        static Mutex _lock = new Mutex ();
+        static object obj = new object ();
+        static ManualResetEvent ManualReset=new ManualResetEvent (false);
+        static int count = 0;
+
+        public static bool IsSingleInstance()
+        {
+            try
+            {
+             
+                Mutex.OpenExisting("demon");
+            }
+            catch 
+            {
+                _m = new Mutex(true, "demon");
+                return true;
+                
+            }
+            return false;
+        }
+
+        public static  void MutexLock()
+        {
+            var list = new List<Task>();
+            for (int i = 0; i < 100; i++)
+            {
+                Thread thread = new Thread(() => 
+                {
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                    //count++;
+                    //lock
+                    //lock (obj)
+                    //{
+                    //    count++;
+                    //}
+                    // Interlock
+                    //Interlocked.Increment(ref count);
+                    //metux
+                    //_lock.WaitOne();
+                    //count++;
+                    //_lock.ReleaseMutex();
+
+                    //manualresetevent
+                    ManualReset.Set();
+                    count++;
+                    ManualReset.Reset ();
+                });
+                thread.IsBackground = true;
+                thread.Start();
+                thread.Join();
+
+               //var task = new Task(()=> {
+               //    lock (obj)
+               //    {
+               //        count++;
+               //    }
+
+                   //
+                  
+                   //ManualReset.WaitOne();
+                   //count++;
+                   //ManualReset.Set();
+
+               //});
+               // task.Start();
+               // list.Add(task);
+            }
+              //Task.WaitAll(list.ToArray());
+            //Task.WhenAll(list).Wait();
+       //     Task.WaitAll(list.ToArray());
+            Console.WriteLine("count :"+count);
+        }
+    }
+}
---
+++ MutexSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using MutexSample;
+using System.Diagnostics;
+//if (!MutexExample.IsSingleInstance())
+//{
+//    Console.WriteLine("more than one instance");
+//}else
+//{
+//    Console.WriteLine("one instance");
+//}
+
+Stopwatch sw = new Stopwatch();
+
+sw.Start();
+MutexExample.MutexLock(); 
+Console.WriteLine(sw.ElapsedMilliseconds);
+sw.Stop();
+Console.ReadLine();
